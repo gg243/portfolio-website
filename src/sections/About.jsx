a572b8c11e2a088e963a1aea5f73627776f56fd1
Damilare Emmanuel <dammygg16@gmail.com>
2025-07-29 11:17:23 +0100

    second grid completely done

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,8 +1,9 @@
 import { section } from "motion/react-client";
-import React from "react";
+import React, { use, useRef } from "react";
 import Card from "../components/card";
 
 function About() {
+  const grid2Container = useRef();
   return (
     <section id="About" className="c-space section-spacing scroll-mt-20">
       <h2 className="text-heading">About Me</h2>
@@ -22,29 +23,52 @@ function About() {
           <div className="absolute inset-x-0 pointer-events-none -bottom-4 h-1/2 sm:h-1/3 bg-gradient-to-t from-indigo" />
         </div>
         <div className="grid-default-color grid-2">
-          <div className="flex items-center justify-center w-full h-full ">
+          <div
+            className="flex items-center justify-center w-full h-full "
+            ref={grid2Container}
+          >
             <p className="flex items-end text-5xl text-grey-500">
               CODE IS CRAFT
             </p>
             <Card
               style={{ rotate: "75deg", top: "30%", left: "20%" }}
               text="GRASP"
+              containerRef={grid2Container}
             />
             <Card
               style={{ rotate: "-30deg", top: "60", left: "45%" }}
               text="SOLID"
+              containerRef={grid2Container}
             />
             <Card
               style={{ rotate: "90deg", bottom: "30%", left: "70%" }}
               text="DESIGN PRINCIPLE"
+              containerRef={grid2Container}
             />
             <Card
               style={{ rotate: "45deg", top: "55%", left: "0%" }}
               text="DESIGN PATTERNS"
+              containerRef={grid2Container}
             />
             <Card
-              style={{ rotate: "20deg", top: "10%", left: "38" }}
+              style={{ rotate: "20deg", top: "10%", left: "38%" }}
               text="SRP"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "30deg", top: "70%", left: "70%" }}
+              image="assets/logos/csharp-pink.png"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "-45deg", top: "70%", left: "25%" }}
+              image="assets/logos/blazor-pink.png"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "-45deg", top: "10%", left: "10%" }}
+              image="assets/logos/dotnet-pink.png"
+              containerRef={grid2Container}
             />
           </div>
         </div>
