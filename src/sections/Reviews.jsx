2fd4124aff508d3e132e972dadf7533fdad3972d
Damilare Emmanuel <dammygg16@gmail.com>
2025-08-06 17:23:33 +0100

    deployment

--- src/sections/Reviews.jsx
+++ src/sections/Reviews.jsx
@@ -4,7 +4,7 @@ import { testimonials } from "../constant";
 const Reviews = () => {
   return (
     <section className="c-space  mt-32">
-      <h2 className="text-3xl"> Testimominals</h2>
+      <h2 className="text-3xl"> Testimominals/ Quotes</h2>
       <AnimatedTestimonials testimonials={testimonials} />
     </section>
   );
